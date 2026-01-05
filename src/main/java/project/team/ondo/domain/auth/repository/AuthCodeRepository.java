52e8c37cab0c8fbc3810b2837ee94d420a2dd1f7
황지훈 <wangjajihooon@gmail.com>
2026-01-05 18:14:11 +0900

    feat: AuthCode, Authentication Repository 추가

---
+++ src/main/java/project/team/ondo/domain/auth/repository/AuthCodeRepository.java
@@ -0,0 +1,16 @@
+package project.team.ondo.domain.auth.repository;
+
+import lombok.NonNull;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import project.team.ondo.domain.auth.entity.AuthCodeEntity;
+
+@Repository
+public interface AuthCodeRepository extends CrudRepository<@NonNull AuthCodeEntity,@NonNull String> {
+
+    AuthCodeEntity findByAuthCode(String code);
+
+    Boolean existsByAuthCode(String code);
+
+    void deleteByAuthCode(String code);
+}
