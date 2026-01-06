5e0c198e652a2cc442c2a721b19edb38afbca977
황지훈 <wangjajihooon@gmail.com>
2026-01-06 17:16:22 +0900

    update: void 타입 오버로드 메소드 추가

--- src/main/java/project/team/ondo/global/response/ApiResponse.java
+++ src/main/java/project/team/ondo/global/response/ApiResponse.java
@@ -6,7 +6,11 @@ public record ApiResponse<T>(
         String message,
         T data
 ) {
-    public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(true, "OK",  data);
+    public static ApiResponse<Void> success(String message) {
+        return new ApiResponse<>(true, message, null);
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
     }
 }
