a393c28efa96dfc36699cd765231d9373eca07bc
황지훈 <wangjajihooon@gmail.com>
2026-01-06 16:04:06 +0900

    feat: attemptLimitExceeded 에러 추가

--- src/main/java/project/team/ondo/global/exception/ErrorCode.java
+++ src/main/java/project/team/ondo/global/exception/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
     //MAIL
     INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "유효하지 않은 인증 코드입니다."),
     VERIFICATION_CODE_EXPIRED(HttpStatus.BAD_REQUEST, "인증 코드가 만료되었습니다."),
+    ATTEMPT_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "인증 시도 횟수를 초과했습니다."),
     EMAIL_ALREADY_VERIFIED(HttpStatus.BAD_REQUEST, "이미 인증된 이메일입니다."),
     EMAIL_NOT_VERIFIED(HttpStatus.BAD_REQUEST, "이메일 인증이 필요합니다."),
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
