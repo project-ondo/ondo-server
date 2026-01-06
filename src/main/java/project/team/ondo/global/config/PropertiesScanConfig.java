84613119cb240288411349718c3a1a3b44fe9cd4
황지훈 <wangjajihooon@gmail.com>
2026-01-06 16:04:21 +0900

    refactor: mail -> email 네이밍 변경

--- src/main/java/project/team/ondo/global/config/PropertiesScanConfig.java
+++ src/main/java/project/team/ondo/global/config/PropertiesScanConfig.java
@@ -4,14 +4,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 import project.team.ondo.global.data.CorsEnvironment;
 import project.team.ondo.global.data.JwtEnvironment;
-import project.team.ondo.global.data.MailEnvironment;
+import project.team.ondo.global.data.EmailEnvironment;
 import project.team.ondo.global.data.RabbitmqEnvironment;
 
 @EnableConfigurationProperties({
         RabbitmqEnvironment.class,
         JwtEnvironment.class,
         CorsEnvironment.class,
-        MailEnvironment.class
+        EmailEnvironment.class
 })
 @Configuration
 public class PropertiesScanConfig {
