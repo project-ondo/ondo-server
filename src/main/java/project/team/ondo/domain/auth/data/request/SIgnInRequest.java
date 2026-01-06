6164989685315bb8714a433d98ad064eff01a7ec
황지훈 <wangjajihooon@gmail.com>
2026-01-06 14:51:30 +0900

    feat: request 필드 추가

--- src/main/java/project/team/ondo/domain/auth/data/request/SIgnInRequest.java
+++ src/main/java/project/team/ondo/domain/auth/data/request/SIgnInRequest.java
@@ -3,7 +3,7 @@ package project.team.ondo.domain.auth.data.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record SIgnInRequest(
-        @NotBlank String uid,
+        @NotBlank String loginId,
         @NotBlank String password
 ) {
 }
