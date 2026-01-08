920e46d942aaa62d234d6a734fd407e88e881fff
황지훈 <wangjajihooon@gmail.com>
2026-01-09 02:12:19 +0900

    refactor: websocketConfig 네이밍 변경

--- src/main/java/project/team/ondo/global/config/WebSocketConfig.java
+++ src/main/java/project/team/ondo/global/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package project.team.ondo.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,12 +12,14 @@ import project.team.ondo.global.data.RabbitmqEnvironment;
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
-public class RabbitmqConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final RabbitmqEnvironment rabbitmqEnvironment;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*");
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
@@ -24,16 +27,23 @@ public class RabbitmqConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.setUserDestinationPrefix("/user");
+
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-");
+        scheduler.initialize();
+
         registry.enableStompBrokerRelay("/topic", "/queue")
                 .setRelayHost(rabbitmqEnvironment.host())
                 .setRelayPort(rabbitmqEnvironment.port())
                 .setClientLogin(rabbitmqEnvironment.username())
                 .setClientPasscode(rabbitmqEnvironment.password())
-                .setSystemLogin(rabbitmqEnvironment.username())
+                .setSystemLogin(rabbitmqEnvironment.host())
                 .setSystemPasscode(rabbitmqEnvironment.password())
                 .setSystemHeartbeatSendInterval(10000)
-                .setSystemHeartbeatReceiveInterval(10000);
-        registry.setUserDestinationPrefix("/user");
+                .setSystemHeartbeatReceiveInterval(10000)
+                .setTaskScheduler(scheduler);
     }
 }
