59ce329f3f1c775950225da9e6c437aaf3182bda
황지훈 <wangjajihooon@gmail.com>
2026-01-08 22:25:12 +0900

    feat: 에러 응답 메소드 추가

--- src/main/java/project/team/ondo/global/response/ApiResponse.java
+++ src/main/java/project/team/ondo/global/response/ApiResponse.java
@@ -1,11 +1,18 @@
 package project.team.ondo.global.response;
 
 
+import project.team.ondo.global.error.CommonErrorCode;
+
 public record ApiResponse<T>(
         boolean success,
         String message,
         T data
 ) {
+
+    public static ApiResponse<Void> error(CommonErrorCode errorCode) {
+        return new ApiResponse<>(false, errorCode.getMessage(), null);
+    }
+
     public static ApiResponse<Void> success(String message) {
         return new ApiResponse<>(true, message, null);
     }
