b5223f315208142a8c1c017f3555890095e386a3
황지훈 <wangjajihooon@gmail.com>
2026-01-08 22:27:16 +0900

    feat: cors, security 설정 추가

---
+++ src/main/java/project/team/ondo/global/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package project.team.ondo.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import project.team.ondo.global.data.CorsEnvironment;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+
+    private final CorsEnvironment corsEnvironment;
+
+    public UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        List<String> allowedOrigins = corsEnvironment.allowedOrigins();
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization","Set-Cookie"));
+        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
