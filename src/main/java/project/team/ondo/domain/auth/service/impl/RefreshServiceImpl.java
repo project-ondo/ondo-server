4cc4f023b0f2ee25cd68cdc1344086aebf497423
황지훈 <wangjajihooon@gmail.com>
2026-01-09 02:13:42 +0900

    feat: 토큰 재발급 서비스 및 구현체 추가

---
+++ src/main/java/project/team/ondo/domain/auth/service/impl/RefreshServiceImpl.java
@@ -0,0 +1,53 @@
+package project.team.ondo.domain.auth.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.team.ondo.domain.auth.data.request.RefreshRequest;
+import project.team.ondo.domain.auth.data.response.AuthTokenResponse;
+import project.team.ondo.domain.auth.exception.InvalidTokenException;
+import project.team.ondo.domain.auth.service.RefreshService;
+import project.team.ondo.domain.user.entity.UserEntity;
+import project.team.ondo.domain.user.exception.UserNotFoundException;
+import project.team.ondo.domain.user.repository.UserRepository;
+import project.team.ondo.global.data.AuthToken;
+import project.team.ondo.global.security.jwt.service.JwtIssueService;
+import project.team.ondo.global.security.jwt.service.JwtParserService;
+import project.team.ondo.global.security.jwt.service.JwtRefreshTokenManagementService;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshServiceImpl implements RefreshService {
+
+    private final JwtParserService jwtParserService;
+    private final UserRepository userRepository;
+    private final JwtIssueService jwtIssueService;
+    private final JwtRefreshTokenManagementService jwtRefreshTokenManagementService;
+
+    @Transactional
+    @Override
+    public AuthTokenResponse execute(RefreshRequest request) {
+        String refreshToken = request.refreshToken();
+
+        if (!jwtParserService.validateRefreshToken(refreshToken)) throw new InvalidTokenException();
+
+        UUID publicId = UUID.fromString(jwtParserService.getUserIdFromRefreshToken(refreshToken));
+
+        UserEntity user = userRepository.findByPublicId(publicId)
+                .orElseThrow(UserNotFoundException::new);
+
+        jwtRefreshTokenManagementService.execute(refreshToken);
+
+        AuthToken newAccessToken = jwtIssueService.issueAccessToken(publicId, user.getRole());
+        AuthToken newRefreshToken = jwtIssueService.issueRefreshToken(publicId);
+
+        return new AuthTokenResponse(
+                newAccessToken.token(),
+                newAccessToken.expiration(),
+                newRefreshToken.token(),
+                newRefreshToken.expiration()
+        );
+    }
+}
