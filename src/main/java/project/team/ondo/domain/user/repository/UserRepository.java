33b6f8dfd50d7599125b9caec8439690399a5408
황지훈 <wangjajihooon@gmail.com>
2026-01-09 02:12:53 +0900

    feat: 유저 레포 메소드 추가

--- src/main/java/project/team/ondo/domain/user/repository/UserRepository.java
+++ src/main/java/project/team/ondo/domain/user/repository/UserRepository.java
@@ -5,7 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.team.ondo.domain.user.entity.UserEntity;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface UserRepository extends JpaRepository<@NonNull UserEntity,@NonNull Long> {
     boolean existsByEmail(String email);
+
+    boolean existsByLoginId(String loginId);
+
+    boolean existsByDisplayName(String displayName);
+
+    Optional<UserEntity> findByLoginId(String loginId);
+
+    Optional<UserEntity> findByPublicId(UUID publicId);
 }
