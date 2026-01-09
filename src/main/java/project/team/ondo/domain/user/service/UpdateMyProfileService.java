63c52d0a437795ca5ea0a39841517ade983f1179
황지훈 <wangjajihooon@gmail.com>
2026-01-10 04:06:38 +0900

    feat: 유저 프로필 업데이트 서비스 및 구현체 추가

---
+++ src/main/java/project/team/ondo/domain/user/service/UpdateMyProfileService.java
@@ -0,0 +1,7 @@
+package project.team.ondo.domain.user.service;
+
+import project.team.ondo.domain.user.data.request.UpdateMyProfileRequest;
+
+public interface UpdateMyProfileService {
+    void execute(UpdateMyProfileRequest request);
+}
