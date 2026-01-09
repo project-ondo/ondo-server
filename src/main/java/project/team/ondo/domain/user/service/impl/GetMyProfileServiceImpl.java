fbab381d6167e94863cb96b6480b5c3a9fad4f9b
황지훈 <wangjajihooon@gmail.com>
2026-01-10 04:05:09 +0900

    feat: 현재 로그인된 사용자 프로필 조회 서비스 및 구현체 추가

---
+++ src/main/java/project/team/ondo/domain/user/service/impl/GetMyProfileServiceImpl.java
@@ -0,0 +1,35 @@
+package project.team.ondo.domain.user.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.team.ondo.domain.user.data.response.MyProfileResponse;
+import project.team.ondo.domain.user.entity.UserEntity;
+import project.team.ondo.domain.user.service.GetMyProfileService;
+import project.team.ondo.global.security.jwt.service.CurrentUserProvider;
+
+@Service
+@RequiredArgsConstructor
+public class GetMyProfileServiceImpl implements GetMyProfileService {
+
+    private final CurrentUserProvider currentUserProvider;
+
+    @Transactional(readOnly = true)
+    @Override
+    public MyProfileResponse execute() {
+        UserEntity user = currentUserProvider.getCurrentUser();
+        return new MyProfileResponse(
+                user.getPublicId(),
+                user.getLoginId(),
+                user.getEmail(),
+                user.getDisplayName(),
+                user.getGender(),
+                user.getMajor(),
+                user.getInterests(),
+                user.getProfileImageUrl(),
+                user.getBio(),
+                user.getRole(),
+                user.getStatus()
+        );
+    }
+}
