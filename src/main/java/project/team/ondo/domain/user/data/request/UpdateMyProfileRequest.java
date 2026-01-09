ee9fbc6e384f2fca3319a3cf2adaeba921af6021
황지훈 <wangjajihooon@gmail.com>
2026-01-10 04:07:33 +0900

    feat: 유저 프로필 업데이트 request 추가

---
+++ src/main/java/project/team/ondo/domain/user/data/request/UpdateMyProfileRequest.java
@@ -0,0 +1,17 @@
+package project.team.ondo.domain.user.data.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import project.team.ondo.domain.user.constant.Gender;
+
+import java.util.List;
+
+public record UpdateMyProfileRequest(
+        @NotBlank @Size(max=20) String displayName,
+        Gender gender,
+        @NotBlank @Size(max=20) String major,
+        List<@NotBlank @Size(max=20) String> interests,
+        String profileImageUrl,
+        @Size(max=500) String bio
+) {
+}
