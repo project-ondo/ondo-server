3a54bac8ee7a3e5dcff546dcd118c16c788f1bbb
황지훈 <wangjajihooon@gmail.com>
2026-01-13 00:08:27 +0900

    feat: 레포 메소드 추가

--- src/main/java/project/team/ondo/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/project/team/ondo/domain/chat/repository/ChatMessageRepository.java
@@ -1,10 +1,13 @@
 package project.team.ondo.domain.chat.repository;
 
 import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.team.ondo.domain.chat.entity.ChatMessageEntity;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<@NonNull ChatMessageEntity, @NonNull Long>, ChatMessageQueryRepository {
+    Page<@NonNull ChatMessageEntity> findAllByRoomIdOrderByIdDesc(Long roomId, Pageable pageable);
 }
--- src/main/java/project/team/ondo/domain/chat/repository/ChatRoomMemberRepository.java
+++ src/main/java/project/team/ondo/domain/chat/repository/ChatRoomMemberRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.team.ondo.domain.chat.entity.ChatRoomMemberEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ChatRoomMemberRepository extends JpaRepository<@NonNull ChatRoomMemberEntity, @NonNull Long> {
     Optional<ChatRoomMemberEntity> findByRoomIdAndUserId(Long roomId, Long userId);
+    List<ChatRoomMemberEntity> findAllByUserIdAndActiveTrue(Long userId);
 }
