9d5c5c4ca1bd0dde1c4c186a731281cd9e2d5f71
황지훈 <wangjajihooon@gmail.com>
2026-01-13 02:37:55 +0900

    update: 중복 제거

--- src/main/java/project/team/ondo/global/config/WebSocketConfig.java
+++ src/main/java/project/team/ondo/global/config/WebSocketConfig.java
@@ -23,9 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*");
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                //.withSockJS(); 운영환경에서 주석 해제
     }
 
     @Override
