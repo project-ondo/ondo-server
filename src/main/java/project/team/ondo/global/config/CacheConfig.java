01aa997b7f275d91ed64b6f88632f65ed9c6ef98
황지훈 <wangjajihooon@gmail.com>
2026-01-13 20:20:11 +0900

    feat: cacheConfig 설정 추가

---
+++ src/main/java/project/team/ondo/global/config/CacheConfig.java
@@ -0,0 +1,20 @@
+package project.team.ondo.global.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(20))
+                .disableCachingNullValues();
+    }
+}
