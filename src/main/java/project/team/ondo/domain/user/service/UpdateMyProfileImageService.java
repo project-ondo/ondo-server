fdf17b50774dcb9805fe03ae218596cec816dda7
황지훈 <wangjajihooon@gmail.com>
2026-01-16 03:47:11 +0900

    feat: 유저 프사 변경 서비스 및 구현체 추가

---
+++ src/main/java/project/team/ondo/domain/user/service/UpdateMyProfileImageService.java
@@ -0,0 +1,7 @@
+package project.team.ondo.domain.user.service;
+
+import project.team.ondo.domain.user.data.request.UpdateMyProfileImageRequest;
+
+public interface UpdateMyProfileImageService {
+    void execute(UpdateMyProfileImageRequest request);
+}
