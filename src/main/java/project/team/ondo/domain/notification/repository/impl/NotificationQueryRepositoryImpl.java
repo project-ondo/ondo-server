7cc1e1639b9fa6472c6233fd0604bb955e267ad7
황지훈 <wangjajihooon@gmail.com>
2026-01-21 06:28:11 +0900

    feat: 알림 커스텀 레포 및 구현체 추가

---
+++ src/main/java/project/team/ondo/domain/notification/repository/impl/NotificationQueryRepositoryImpl.java
@@ -0,0 +1,29 @@
+package project.team.ondo.domain.notification.repository.impl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import project.team.ondo.domain.notification.entity.QNotificationEntity;
+import project.team.ondo.domain.notification.repository.NotificationQueryRepository;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationQueryRepositoryImpl implements NotificationQueryRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private final QNotificationEntity notification = QNotificationEntity.notificationEntity;
+
+    @Override
+    public long markAllRead(UUID receiverPublicId) {
+        return jpaQueryFactory
+                .update(notification)
+                .set(notification.read, true)
+                .where(
+                        notification.receiverPublicId.eq(receiverPublicId),
+                        notification.read.eq(false)
+                )
+                .execute();
+    }
+}
