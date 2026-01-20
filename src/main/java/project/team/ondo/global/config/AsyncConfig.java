e12796834ea0eecfcb92f541985467d99b01dbac
황지훈 <wangjajihooon@gmail.com>
2026-01-21 06:22:29 +0900

    feat: async 설정 추가

---
+++ src/main/java/project/team/ondo/global/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package project.team.ondo.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
