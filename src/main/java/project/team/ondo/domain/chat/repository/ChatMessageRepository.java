bf580df17d7d0c03299134cd5a8751f0c826e46e
황지훈 <wangjajihooon@gmail.com>
2026-01-22 18:31:31 +0900

    feat: 최신 메시지 조회 메소드 추가

--- src/main/java/project/team/ondo/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/project/team/ondo/domain/chat/repository/ChatMessageRepository.java
@@ -7,9 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.team.ondo.domain.chat.entity.ChatMessageEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface ChatMessageRepository extends JpaRepository<@NonNull ChatMessageEntity, @NonNull Long>, ChatMessageQueryRepository {
     Page<@NonNull ChatMessageEntity> findAllByRoomIdOrderByIdDesc(Long roomId, Pageable pageable);
 
     Page<@NonNull ChatMessageEntity> findAllByRoomIdAndIdLessThanOrderByIdDesc(Long roomId, Long cursor, Pageable pageable);
+
+    Optional<ChatMessageEntity> findTopByRoomIdOrderByIdDesc(Long roomId);
 }
