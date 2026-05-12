47cf01358e9701d4cbe5e808cb9d15f93fe6dade
황지훈 <wangjajihooon@gmail.com>
2026-05-12 19:35:52 +0900

    fix(chat): 탈퇴한 상대에게 메시지 전송 시 CHAT_OPPONENT_WITHDRAWN 예외 반환

--- src/main/java/project/team/ondo/domain/chat/service/impl/SendMessageServiceImpl.java
+++ src/main/java/project/team/ondo/domain/chat/service/impl/SendMessageServiceImpl.java
@@ -10,10 +10,12 @@ import project.team.ondo.domain.chat.entity.ChatMessageOutboxEntity;
 import project.team.ondo.domain.chat.entity.ChatRoomEntity;
 import project.team.ondo.domain.chat.entity.ChatRoomMemberEntity;
 import project.team.ondo.domain.chat.event.ChatMessageSentEvent;
+import project.team.ondo.domain.chat.exception.ChatOpponentWithdrawnException;
 import project.team.ondo.domain.chat.exception.ChatRoomMemberInactiveException;
 import project.team.ondo.domain.chat.exception.ChatRoomMemberNotFoundException;
 import project.team.ondo.domain.chat.exception.ChatRoomNotFoundException;
 import project.team.ondo.domain.chat.exception.UserChatBlockedException;
+import project.team.ondo.domain.user.constant.UserStatus;
 import project.team.ondo.domain.chat.repository.ChatMessageOutboxRepository;
 import project.team.ondo.domain.chat.repository.ChatMessageRepository;
 import project.team.ondo.domain.chat.repository.ChatRoomMemberRepository;
@@ -52,6 +54,11 @@ public class SendMessageServiceImpl implements SendMessageService {
         }
 
         Long opponentId = chatRoom.getUserAId().equals(me.getId()) ? chatRoom.getUserBId() : chatRoom.getUserAId();
+
+        UserEntity opponent = userRepository.findById(opponentId).orElseThrow(ChatRoomMemberNotFoundException::new);
+        if (opponent.getStatus() == UserStatus.DELETED) {
+            throw new ChatOpponentWithdrawnException();
+        }
 
         ChatRoomMemberEntity opponentMember = chatRoomMemberRepository.findByRoomIdAndUserId(chatRoom.getId(), opponentId)
                 .orElseThrow(ChatRoomMemberNotFoundException::new);
