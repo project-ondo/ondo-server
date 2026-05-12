10106566642581180088655f90ea5fff7dc8c3f5
황지훈 <wangjajihooon@gmail.com>
2026-05-12 18:13:39 +0900

    fix(chat): LeaveRoom 삭제 로직을 루프 단건 삭제에서 Bulk DELETE로 교체

--- src/main/java/project/team/ondo/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/project/team/ondo/domain/chat/repository/ChatMessageRepository.java
@@ -4,6 +4,9 @@ import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import project.team.ondo.domain.chat.entity.ChatMessageEntity;
 
@@ -19,5 +22,7 @@ public interface ChatMessageRepository extends JpaRepository<@NonNull ChatMessag
 
     boolean existsByRoomId(Long roomId);
 
-    void deleteAllByRoomId(Long roomId);
+    @Modifying
+    @Query("DELETE FROM ChatMessageEntity m WHERE m.roomId = :roomId")
+    void deleteAllByRoomId(@Param("roomId") Long roomId);
 }
