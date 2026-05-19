db0ab3051f1b778bdf66758e4c98c24257ada7ea
황지훈 <wangjajihooon@gmail.com>
2026-05-19 14:20:08 +0900

    update(notification): dndStart, dndEnd 필드 Swagger 설명 추가

--- src/main/java/project/team/ondo/domain/notification/data/request/UpdateMyNotificationSettingRequest.java
+++ src/main/java/project/team/ondo/domain/notification/data/request/UpdateMyNotificationSettingRequest.java
@@ -1,5 +1,6 @@
 package project.team.ondo.domain.notification.data.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalTime;
 
 public record UpdateMyNotificationSettingRequest(
@@ -7,7 +8,9 @@ public record UpdateMyNotificationSettingRequest(
         Boolean chatEnabled,
         Boolean communityEnabled,
         Boolean dndEnabled,
+        @Schema(description = "방해 금지 시작 시간 (HH:mm:ss)", example = "22:00:00")
         LocalTime dndStart,
+        @Schema(description = "방해 금지 종료 시간 (HH:mm:ss)", example = "08:00:00")
         LocalTime dndEnd
 ) {
 }
